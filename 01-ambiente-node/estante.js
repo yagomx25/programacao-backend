a846e7fc56c7ae2585e9ab7e94d3a0a4b9802c8e
Yago Henrique <yago.r@edu.senai.br>
2026-07-31 15:05:03 -0300

    feat: adiciona atividade minha estante

---
+++ 01-ambiente-node/estante.js
@@ -0,0 +1,18 @@
+// ===== LIVRO 1 =====
+const livro1 = {
+  titulo: "Clean Code",
+  autor: "Robert C. Martin",
+  preco: 89.9,
+  estoque: 12,
+};
+
+// ===== LIVRO 2 — Já preenchido para você =====
+const livro2 = {
+  titulo: "O Senhor dos Aneis",
+  autor: "J.R.R. Tolkien",
+  preco: 55.90,
+  estoque: 3,
+};
+
+// Esta linha entrega os dois livros para os outros arquivos usarem
+module.exports = { livro1, livro2 };
